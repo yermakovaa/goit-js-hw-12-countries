0c18fe0658c2feb1af08bc22cfc456d15b199fac
Anastasiia Yermakova <yermak1037@gmail.com>
2020-11-02 21:27:39 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,5 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2/name';
 
 export default function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/${searchQuery}`)
-    .then(response => response.json())
-    .catch(err => console.log(err));
+  return fetch(`${BASE_URL}/${searchQuery}`).then(response => response.json());
 }
